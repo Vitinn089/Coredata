f49ab8da7b1cfd48a201b9151c363351343e00c5
Vitinn089 <vitorlopes089@gmail.com>
2023-03-19 00:34:36 -0300

    refact: Refactor routes.
    Centralized the routes in vitinn route.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import acessLog from './middlewares/acessLog';
 import handleCors from './middlewares/handleCors';
 import vitinn from './routes/vitinn';
-import updateConfig from './routes/updateConfig';
 import appConfigs from './config/config';
 
 const app = express();
@@ -14,7 +13,6 @@ app.use(cors());
 app.use(acessLog);
 app.use(express.json());
 app.use('/api/vitinn', vitinn);
-app.use('/api/update-config', updateConfig);
 
 
 app.listen(PORT, () => console.log(`\nServer is running in: http://localhost:${PORT}/` ));
--- src/routes/vitinn.ts
+++ src/routes/vitinn.ts
@@ -1,6 +1,8 @@
 import { express } from '..';
 import { PostgresProjectRepository } from '../repositories/postgres/postgres-project-repository';
+import { CreateProjectUseCases } from '../use-cases/create-projects-use-cases';
 import { GetProjectsUseCases } from '../use-cases/get-projects-use-cases';
+import { getDataApi } from '../utils/getDataApi';
 
 const router = express.Router();
 
@@ -10,6 +12,25 @@ router.get('/', async (req, res) => {
 
 	res.status(200);
 	res.json(await getProjectsUseCases.execute());
+});
+
+// Rota para atualizar  o banco de dados com base nas configuraçõesl
+router.get('/update-config',  async (req, res) => {
+	try {
+		const postgresProjectRepository = new PostgresProjectRepository();
+		const createProjectsUseCases = new CreateProjectUseCases(postgresProjectRepository);
+
+		getDataApi().then(async projects => {
+			for (const project of projects) {
+				await createProjectsUseCases.execute(project);
+			}
+			console.info('Refresh concluido.');
+			return res.status(201).send();
+		});
+	} catch (error) {
+		console.error(error);
+		return res.status(500).send();
+	}
 });
 
 export default router;
